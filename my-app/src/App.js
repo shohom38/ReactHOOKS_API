f10062039e709c727fdc3a3ad7242f74578320f8
sumin <shohom38@naver.com>
2022-01-01 00:38:04 +0900

    useRef #3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import InputExam from './Components/InputExam';
 
 function App() {
     return (
-        <div className="App">
-            <h1>Hello</h1>
-        </div>
+        <InputExam />
     );
 }
 
